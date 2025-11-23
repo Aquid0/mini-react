f7d55916ec7282d7c9fbd8989af4265befa86e02
Matthew <aquidmatthewla@gmail.com>
2025-11-23 02:30:59 +0000

    notes

--- src/hooks.ts
+++ src/hooks.ts
@@ -36,6 +36,16 @@ export function useState(initial: any) {
   return [hook.state, setState];
 }
 
+
+// reducer(state, action): specifies how the state gets updated, should take the 'state' and 'action' as arguments, and should return the next state
+// initialArg: the value from which the initial state is calculated
+// init: If it's specified, the initial state is set to the result of of calling init(initialArg)
+
+// returns: 
+// state - current state of this state variable
+// dispatch(action) - function that lets you update the state to a different value and trigger a re-render. You need to pass the action
+// as the only argument to the dispatch function. Should set the next state to the result of calling the reducer that has been provided with the 
+// current state and the action that has been passed into it. 
 export function useReducer(reducer: any, initialArg: any, init?: any) {
   // TODO
 }
